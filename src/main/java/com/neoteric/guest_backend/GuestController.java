266496a6c31dc7e7a0e6f15ca4cfdad87267cfa8
susheel2405 <susheelchinnup23@gmail.com>
2025-08-04 17:26:04 +0530

    initial commit

--- src/main/java/com/neoteric/guest_backend/GuestController.java
+++ src/main/java/com/neoteric/guest_backend/GuestController.java
@@ -1,33 +1,33 @@
 package com.neoteric.guest_backend;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/guests")
 @CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api")
 public class GuestController {
 
     @Autowired
     private GuestRepository guestRepository;
 
+    @PostMapping("/guests")
+    public ResponseEntity<List<Guest>> addGuests(@RequestBody List<Guest> guests) {
+        List<Guest> savedGuests = guestRepository.saveAll(guests);
+        return ResponseEntity.ok(savedGuests);
+    }
 
-
-
-    @GetMapping("/all")
+    @GetMapping("/get/guests")
     public List<Guest> getGuests() {
         return guestRepository.findAll();
     }
 
-
-    @PostMapping("/add")
-    public List<Guest> addGuests(@RequestBody List<Guest> guests) {
-        return guestRepository.saveAll(guests); // ✅ appends to DB
+    @DeleteMapping("/guests/{id}")
+    public void deleteGuest(@PathVariable Long id) {
+        guestRepository.deleteById(id);
     }
-
-
-
 }
 
